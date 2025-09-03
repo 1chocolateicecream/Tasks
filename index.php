b480a22b8e5248c6a5c2ebd52b3dd1805354a673
unknown <kvelps@gmail.com>
2025-09-03 13:53:09 +0300

    5 uzd

--- index.php
+++ index.php
@@ -4,13 +4,29 @@ while (true) {
 
     if ($input == "n") {
         break;
-    } else {
-        echo "$input\n";
     }
+    // else {
+    //     echo "$input\n";
+    // }
 
     $tasks = [
         "task 1",
         "task 2",
         "task 3"
     ];
+
+    switch ($input) {
+        case '1':
+            echo "tasks list\n";
+            break;
+        case 'a':
+            echo "add task\n";
+            break;
+        case 'r':
+            echo "remove task\n";
+            break;
+        default:
+            echo "something went wrong\n";
+            break;
+    }
 }
