7d3f78fe533fd135879d1ecf37ad373aadf8b878
unknown <kvelps@gmail.com>
2025-09-08 10:15:33 +0300

    second commit

--- index1.php
+++ index1.php
@@ -1,27 +1,36 @@
 <?php
 
-// echo "something\n";
-
 $tasks = ["task 1", "task 2", "task 3"];
 
 function showAllTasks($inputTasks)
 {
+    echo "=== List of all tasks ===\n";
     foreach ($inputTasks as $task) {
         echo $task . "\n";
     }
+    echo "==================\n\n";
+}
+
+function addNewTask(&$inputTasks){
+    $newTask = readline("enter new task: ");
+    $inputTasks[] = $newTask;
 }
 
 while (true) {
+    echo "Task Manager\n";
+    echo "0 - exit\n";
+    echo "1 - Show all tasks\n";
+    echo "2 - Add new task\n";
     $input = readline("some user input(0 or 1): ");
-    if ($input == "n") {
-        exit;
-    }
 
     switch ($input) {
-        case 0:
+        case 0 or "n":
             exit;
         case 1:
             showAllTasks($tasks);
+            break;
+        case 2:
+            addNewTask($tasks);
             break;
     }
 }
